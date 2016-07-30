af84b3cc7033290c677263122e62de0755d1cd70
GIT ADMIN <kat.bana204@gmail.com>
2016-07-30 18:34:30 +0200

    add jsonm gist

--- scripts/app.js
+++ scripts/app.js
@@ -28,8 +28,8 @@ angular
             contributors: function($http) {
               //https://api.github.com/gists/bcea5b70693143a7f696f3542b88edf7
               var promise = $http.get('https://gist.githubusercontent.com/204Constie/bcea5b70693143a7f696f3542b88edf7/raw/304ae65ef3c89696fb3d171aabca494c5f394c8b/xfcontributors', { headers: {'Content-Type': 'application/json'} }).then(function(response){
-              
-              console.log(response.data.files.xfcontributors.content);
+              //response.data.files.xfcontributors.content
+              console.log(response.data);
               return response.data.files.xfcontributors.content;
             });
             return promise;
