29e6378051c73414c585233c59d118190c3216d6
GIT ADMIN <kat.bana204@gmail.com>
2016-07-30 17:44:40 +0200

    gist data controbutors

--- scripts/app.js
+++ scripts/app.js
@@ -28,7 +28,7 @@ angular
             contributors: function($http) {
               var promise = $http.get('https://api.github.com/gists/bcea5b70693143a7f696f3542b88edf7').then(function(response){
               
-              console.log(response.data.files.xfcontributors.content);
+              console.log(response.data.files);
               return response.data.files.xfcontributors.content;
             });
             return promise;
